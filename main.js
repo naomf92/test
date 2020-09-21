9df6f512a4a6b915b5dac0bc9c44277c7c937cb7
naomi <iam.naomi92@gmail.com>
2020-09-21 21:26:54 +0900

    test

---
+++ main.js
@@ -0,0 +1,32 @@
+const modal = document.getElementById('easyModal');
+
+const openBtn = document.getElementById('modalOpen');
+
+const closeBtn = document.getElementsByClassName('modalClose')[0];
+
+// クリックで開くイベントリスナー
+openBtn.addEventListener('click', modalOpen);
+
+// クリックで閉じるイベントリスナー
+closeBtn.addEventListener('click', modalClose);
+
+// モーダル以外の場所クリックのイベントリスナー
+window.addEventListener('click', clickAnywhere);
+
+// モーダルオープンの関数
+function modalOpen() {
+    modal.style.display = 'block';//開くボタンを押したらモーダルが出る
+}
+
+// モーダルを閉じる関数
+function modalClose() {
+    modal.style.display = 'none';//閉じるボタンを押したらモーダルが消える
+}
+
+// モーダル以外がクリックされた時に閉じる関数
+function clickAnywhere(e) {// 以下のeventは引数として扱うので、宣言時に用意も必要です
+    if(e.target == modal) {
+        modal.style.display = 'none';
+        console.log(e);
+    }
+}
