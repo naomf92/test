ceeef91f8836cc9a903a9eadfb928cf849cb632f
naomi <iam.naomi92@gmail.com>
2020-10-04 15:37:01 +0900

    test

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// const modal = document.getElementById('easyModal');//モーダル要素
+//const modal = document.getElementById('easyModal');//モーダル要素
 const modal = document.querySelector('.modal');
-// const modal = document.querySelector(`.modal[data-name="${}]`);
+//const modal = document.querySelector(`.modal[data-name="${}]`);
 
-// const openBtn = document.getElementById('modalOpen');//モーダルを出すボタン要素
+//const openBtn = document.getElementById('modalOpen');//モーダルを出すボタン要素
 const openBtn = document.querySelector('.btn');
 
 const closeBtn = document.getElementsByClassName('modalClose')[0];
@@ -19,7 +19,7 @@ window.addEventListener('click', clickAnywhere);
 // モーダルオープンの関数
 function modalOpen() {
     modal.style.display = 'block';//開くボタンを押したらモーダルが出る
-    console.log(modal);
+    console.log(modal.dataset.name);
 }
 
 // モーダルを閉じる関数
