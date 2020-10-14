39b4488024780a0fa404323d3e150b1b088d7ef2
naomi <iam.naomi92@gmail.com>
2020-10-14 18:34:37 +0900

    test

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
 const modal =  document.getElementsByClassName('modal')//モーダル要素
 //console.log(modal);
 
-//const openBtn = document.getElementById('modalOpen');//モーダルを出すボタン要素
-const openBtn = document.getElementsByClassName('btn');
-console.log(openBtn);
+const openBtn = document.getElementsByClassName('btn');//モーダルを出すボタン要素
+//console.log(openBtn);
 
-// const closeBtn = document.getElementsByClassName('modalClose')[0];
+//const closeBtn = document.getElementsByClassName('modalClose');
 
 // クリックで開くイベントリスナー
 openBtn.addEventListener('click', modalOpen);
 
 // クリックで閉じるイベントリスナー
-// closeBtn.addEventListener('click', modalClose);
+//closeBtn.addEventListener('click', modalClose);
 
 // モーダル以外の場所クリックのイベントリスナー
 // window.addEventListener('click', clickAnywhere);
 
 // モーダルオープンの関数
 function modalOpen() {
-    // modal.style.display = 'block';//開くボタンを押したらモーダルが出る
+    //if() {
+        modal[0].classList.add('modalOpen');
+    //} 
+    //modal.style.display = 'block';//開くボタンを押したらモーダルが出る
 }
 
 // モーダルを閉じる関数
-// function modalClose() {
-//     modal.style.display = 'none';//閉じるボタンを押したらモーダルが消える
-// }
+//function modalClose() {
+    //modal.classList.remove('modalOpen');
+    //modal.style.display = 'none';//閉じるボタンを押したらモーダルが消える
+//}
 
 // // モーダル以外がクリックされた時に閉じる関数
 // function clickAnywhere(e) {// 以下のeventは引数として扱うので、宣言時に用意も必要です
