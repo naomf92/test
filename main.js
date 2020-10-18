8a2bc9eb00fdcee365508a8327ce6667f3a974fb
naomi <iam.naomi92@gmail.com>
2020-10-18 18:34:33 +0900

    test

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const modal =  document.getElementsByClassName('modal')//モーダル要素
-//console.log(modal);
+console.log(modal);
 
 const openBtn = document.getElementsByClassName('btn')[0];//モーダルを出すボタン要素
 //console.log(openBtn);
@@ -17,8 +17,8 @@ openBtn.addEventListener('click', modalOpen);
 
 // モーダルオープンの関数
 function modalOpen() {
-    if () {//押したボタンが一つ目のボタンなら → 「何かしらのボタンが押されたら」の方が再利用性がありますね
-        modal[0].classList.add('modalOpen');//開くボタンを押したら一つ目のモーダルが出る
+    if () {//何かしらのボタンが押されたら
+        modal[0].classList.add('modalOpen');//[i]個目のモーダルが出る
     } else if () {//押したボタンが二つ目のボタンなら → 3つ目、4つ目と増えるとその分else ifが増えるので、そうならないようにするには、配列のインデックス番号を[i]など、仮に値が入る変数として用意するのが良さそうです（再利用性の高さを意識）
     }
 }
