710afcadded20f6b0de4202a97f4a5680b4aa6f9
naomi <iam.naomi92@gmail.com>
2020-11-19 20:45:36 +0900

    test

--- main.js
+++ main.js
@@ -15,27 +15,42 @@ function modalOpen() {
         }
         openBtn[i].addEventListener('click', function(e) {
             open(e);});
+
+
+        function close() {
+            modal[i].classList.remove('is-open');
+            modal[i].classList.add('is-close');
+
+            setTimeout(function() {
+            modal[i].classList.remove('is-close')
+            }, 1000);
+        }
+        closeBtn[i].addEventListener('click', function(e) {
+            close(e);});
+
+        modal[i].addEventListener('click',function(e) {
+            close(e)});
     }
 }
 modalOpen();
 
-function modalClose() {
-    for(let i = 0; i < modal.length; i++){
-        modal[i].classList.remove('is-open');
-        modal[i].classList.add('is-close');
+// function modalClose() {
+//     for(let i = 0; i < modal.length; i++){
+//         modal[i].classList.remove('is-open');
+//         modal[i].classList.add('is-close');
 
-        setTimeout(function() {
-            modal[i].classList.remove('is-close')
-        }, 1000);
+//         setTimeout(function() {
+//             modal[i].classList.remove('is-close')
+//         }, 1000);
 
-        closeBtn[i].addEventListener('click', function(e) {
-            modalClose(e);});
+//         closeBtn[i].addEventListener('click', function(e) {
+//             modalClose(e);});
 
-        modal[i].addEventListener('click',function(e) {
-            modalClose(e)});
-    }
-}
-modalClose();
+//         modal[i].addEventListener('click',function(e) {
+//             modalClose(e)});
+//     }
+// }
+// modalClose();
 
 // function clickAnywhere() {
 //     for(let i = 0; i < modal.length; i++) {
