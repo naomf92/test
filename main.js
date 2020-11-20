9502b779d757efa50c163976e818e5c122d9a139
naomi <iam.naomi92@gmail.com>
2020-11-20 21:34:30 +0900

    test

--- main.js
+++ main.js
@@ -4,58 +4,62 @@ const openBtn = document.getElementsByClassName('btn');
 
 const closeBtn = document.querySelectorAll('.modalClose');
 
-function modalOpen() {
+const modalOpen = () => {
     for(let i = 0; i < modal.length; i++){
 
-        function open(e) {
+        const open = (e) => {
             let dataModalOpen = e.currentTarget.getAttribute('data-name');
             if (modal[i].getAttribute('data-name') === dataModalOpen) {
                 modal[i].classList.add('is-open');
             }
-        }
+        };
         openBtn[i].addEventListener('click', function(e) {
             open(e);});
 
-
-        function close() {
+        const close = () => {
             modal[i].classList.remove('is-open');
             modal[i].classList.add('is-close');
 
             setTimeout(function() {
             modal[i].classList.remove('is-close')
             }, 1000);
-        }
+        };
         closeBtn[i].addEventListener('click', function(e) {
             close(e);});
 
         modal[i].addEventListener('click',function(e) {
             close(e)});
     }
-}
+
+};
 modalOpen();
 
-// function modalClose() {
+// function modalOpen() {
 //     for(let i = 0; i < modal.length; i++){
-//         modal[i].classList.remove('is-open');
-//         modal[i].classList.add('is-close');
 
-//         setTimeout(function() {
+//         function open(e) {
+//             let dataModalOpen = e.currentTarget.getAttribute('data-name');
+//             if (modal[i].getAttribute('data-name') === dataModalOpen) {
+//                 modal[i].classList.add('is-open');
+//             }
+//         }
+//         openBtn[i].addEventListener('click', function(e) {
+//             open(e);});
+
+
+//         function close() {
+//             modal[i].classList.remove('is-open');
+//             modal[i].classList.add('is-close');
+
+//             setTimeout(function() {
 //             modal[i].classList.remove('is-close')
-//         }, 1000);
-
+//             }, 1000);
+//         }
 //         closeBtn[i].addEventListener('click', function(e) {
-//             modalClose(e);});
+//             close(e);});
 
 //         modal[i].addEventListener('click',function(e) {
-//             modalClose(e)});
+//             close(e)});
 //     }
 // }
-// modalClose();
-
-// function clickAnywhere() {
-//     for(let i = 0; i < modal.length; i++) {
-//         modal[i].addEventListener('click',function(e) {
-//         modalClose(e)});
-//     }
-// }
-// window.addEventListener('click', clickAnywhere);
+// modalOpen();
