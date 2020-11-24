d1b1da2e29501fb739a8953d68be8b5eb295923a
naomi <iam.naomi92@gmail.com>
2020-11-24 18:11:41 +0900

    close #3

--- main.js
+++ main.js
@@ -33,33 +33,3 @@ const modalOpen = () => {
 
 };
 modalOpen();
-
-// function modalOpen() {
-//     for(let i = 0; i < modal.length; i++){
-
-//         function open(e) {
-//             let dataModalOpen = e.currentTarget.getAttribute('data-name');
-//             if (modal[i].getAttribute('data-name') === dataModalOpen) {
-//                 modal[i].classList.add('is-open');
-//             }
-//         }
-//         openBtn[i].addEventListener('click', function(e) {
-//             open(e);});
-
-
-//         function close() {
-//             modal[i].classList.remove('is-open');
-//             modal[i].classList.add('is-close');
-
-//             setTimeout(function() {
-//             modal[i].classList.remove('is-close')
-//             }, 1000);
-//         }
-//         closeBtn[i].addEventListener('click', function(e) {
-//             close(e);});
-
-//         modal[i].addEventListener('click',function(e) {
-//             close(e)});
-//     }
-// }
-// modalOpen();
